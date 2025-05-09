183306f14acf5de74627d618f805987e96985c16
Roger Aspelin <raspelin69@gmail.com>
2025-05-09 19:35:49 +0200

    Move font and CSS setup to root layout

--- frontend/src/app/(public)/layout.tsx
+++ frontend/src/app/(public)/layout.tsx
@@ -1,12 +1,7 @@
 'use client';
 
 import { Toaster } from 'sonner';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { RegistrationProvider } from '@/contexts/RegistrationContext';
-import './../globals.css';
-
-const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
-const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] });
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
