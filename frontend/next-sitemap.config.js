2e341c3cdd61bdef4342100503fb6515a9311f17
Roger Aspelin <raspelin69@gmail.com>
2025-05-17 14:51:51 +0200

    meta update

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://evlink.se',
+  siteUrl: 'https://evlinkha.se',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
